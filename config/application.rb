b674da4532c9844a82aecde07258de0ddc415065
mrmrtmrn <mrmrtmrn@gmail.com>
2023-10-09 00:53:13 +0900

    rspec設定変更

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module RailsSandbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
